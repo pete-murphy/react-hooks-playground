4f6eaf5e425c7e9de7a8dceba55c38fd939cf7a8
Peter Murphy <peter.francis.murphy@gmail.com>
2018-11-20 22:23:02 -0500

    Many changes, now CRA template not Parcel

--- src/App.js
+++ src/App.js
@@ -1,3 +1,50 @@
 import React from "react"
+import { prop } from "ramda"
+import useProfunctorState from "@staltz/use-profunctor-state"
 
-export default () => <div>Hello world!</div>
+const Input = ({ state, setState }) => {
+  const handleChange = ({ target: { value } }) =>
+    setState(value)
+
+  return (
+    <div>
+      <input value={state} onChange={handleChange} />
+    </div>
+  )
+}
+
+function App() {
+  const initialState = {
+    name: "Moo",
+    other: {},
+    isValid: false
+  }
+  const { state, promap } = useProfunctorState(initialState)
+
+  const inputP = promap(
+    prop("name"),
+    (fromComponent, state) => ({
+      ...state,
+      name: fromComponent,
+      isValid: /i+/gi.test(fromComponent)
+    })
+  )
+
+  return (
+    <div>
+      <div>Global app state: {JSON.stringify(state)}</div>
+      <Input {...inputP} />
+      <Input
+        {...inputP.promap(x => x, x => x.toUpperCase())}
+      />
+      <Input
+        {...inputP.promap(
+          x => x.toLowerCase(),
+          x => x.toUpperCase()
+        )}
+      />
+    </div>
+  )
+}
+
+export default App
